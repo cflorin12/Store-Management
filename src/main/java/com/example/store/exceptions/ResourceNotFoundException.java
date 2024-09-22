d4aceacd721551992b50f841291af5d657d390ea
Florin Buha <florinbuha15@yahoo.com>
2024-09-22 22:03:01 +0300

    Logging

--- src/main/java/com/example/store/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/example/store/exceptions/ResourceNotFoundException.java
@@ -1,7 +1,14 @@
 package com.example.store.exceptions;
 
+import com.example.store.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ResourceNotFoundException extends RuntimeException {
+
+    private static Logger logger = LoggerFactory.getLogger(ResourceNotFoundException.class);
     public ResourceNotFoundException(String message) {
         super(message);
+        logger.error(message);
     }
 }
--- src/main/java/com/example/store/service/ProductService.java
+++ src/main/java/com/example/store/service/ProductService.java
@@ -26,15 +26,19 @@ public class ProductService {
     }
 
     public void delete(String id) {
+        logger.info("Received request to delete product with ID: {}", id);
         ObjectId objectId = new ObjectId(id);
         if (!productRepository.existsById(objectId)) {
             throw new ResourceNotFoundException("Product with id " + id + " does not exist");
         }
         productRepository.deleteById(objectId);
+        logger.info("Product with ID {} has been successfully deleted",id);
     }
 
     public Product update(ObjectId id, Map<String,Object> newData){
+        logger.info("Received request to update product with ID: {}", id);
         Product existingProduct = productRepository.findById(id).orElseThrow();
+        logger.debug("Found product {}",existingProduct.toString());
         newData.forEach((key,value) ->{
             switch (key){
                 case "name":
@@ -55,6 +59,7 @@ public class ProductService {
                     break;
             }
         });
+        logger.debug("New product {}",existingProduct.toString());
         return productRepository.save(existingProduct);
     }
 }
